50f153987b1a855769d219143a60bceabc02f5cd
maxflex <makcyxa-k@yandex.ru>
2020-02-23 06:12:41 +0300

    Webmanifest

---
+++ front/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: "My PTS",
+    short_name: "My PTS",
+    theme_color: "#f3377a",
+    background_color: "#ffffff",
+    display: "fullscreen",
+  },
+}
