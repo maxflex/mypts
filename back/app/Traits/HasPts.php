6132ed73226e362e2e54827399a03f583f1c1790
maxflex <makcyxa-k@yandex.ru>
2020-04-06 04:59:48 +0300

    Display linebreaks

--- back/app/Traits/HasPts.php
+++ back/app/Traits/HasPts.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 trait HasPts
 {
+    protected $appends = ['desc_html'];
+
     public function getDescHtmlAttribute()
     {
         return str_replace("\n", "<br />", $this->attributes['desc']);
